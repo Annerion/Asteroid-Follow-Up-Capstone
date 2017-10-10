1420b4bf76f59ea570bb5c3947e6b2db2adef9e8
Annerion <krd311@nyu.edu>
2017-10-10 14:18:02 -0500

    Same as before.

--- IRAF/data_generator.py
+++ IRAF/data_generator.py
@@ -21,10 +21,9 @@ def clear():
 		for x in range(0, width):
 			image.data[y][x]=0
 def placeStar(A,s,x0,y0,cutoff=10):
-#	print "Place: ", x0, y0
 	for y in range(int(y0-cutoff*s), int(y0+cutoff*s)):
 		for x in range(int(x0-cutoff*s), int(x0+cutoff*s)):
-			if (0<=x<width)&(0<=y<height):
+			if (0<=x<width) and (0<=y<height):
 				image.data[y][x]+=int(A*math.exp(-1*((x-x0)**2+(y-y0)**2)/(2*s**2)))
 def makeField(count=100):
 	for i in range(int(count)-1):
@@ -39,22 +38,18 @@ def smear():
 	image.data/=steps
 	for y in range(0, height-1):
 		for x in range(0, width-1):
-#			print "Smear", x, y
 			for s in range(1,int(steps)):
-				if (x+s<width)&(y+s<height):
+				if (x+s<width) and (y+s<height):
 					image.data[y][x]+=image.data[y+s*math.sin(angle)][x+s*math.cos(angle)]
 def recondense():
-	star_map= [[0] * int(width/expansion_factor) for i in range(int(height/expansion_factor))]
+	star_map= [[1] * int(width/expansion_factor) for i in range(int(height/expansion_factor))]
 	for y in range(0, (height-1)/expansion_factor):
 		for x in range(0, (width-1)/expansion_factor):
-#			print "recondense: ", x, y
-			for yp in range (y*expansion_factor, (y+1)*expansion_factor-1):
-				for xp in range (x*expansion_factor, (x+1)*expansion_factor-1):
+			for yp in range (y*expansion_factor, (y+1)*expansion_factor):
+				for xp in range (x*expansion_factor, (x+1)*expansion_factor):
 					if yp<height and xp<width:
 						star_map[y][x]+=noise(image.data[yp][xp])
-#						star_map[y][x]+=image.data[yp][xp]
 	image.data= star_map
-#	print star_map
 #clear()
 makeField()
 smear()
